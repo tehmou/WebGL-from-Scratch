4a9954c53c0a6a7931df860c7cd4a4320193f22e
Timo Tuominen <timo.tuominen@gmail.com>
2012-02-10 23:45:58 +0100

    Refactory build script a bit.

--- Jakefile.js
+++ Jakefile.js
@@ -1,131 +1,71 @@
-var fs = require("fs");
-var exec = require('child_process').exec;
+(function () {
 
-var SRC_DIR = "src";
-var BUILD_DIR = "build";
-var SAMPLES_DIR = BUILD_DIR + "/samples";
+    var fs = require("fs");
+    var exec = require("child_process").exec;
+    var builder = require("./builder.js");
 
+    var SRC_DIR = "src";
+    var BUILD_DIR = "build";
+    var SAMPLES_DIR = BUILD_DIR + "/samples";
 
-task ("default", ["reset-build-directory", "process-samples", "clean-samples"], function () {
-    startLog("default");
+    desc("Triggers a build.");
+    task ("default", ["build"]);
 
-    endLog();
-});
+    desc("Build the samples into the build directory.");
+    task("build", ["clean", "copy-samples", "process-samples"]);
 
-task ("reset-build-directory", function () {
-    startLog("reset-build-directory");
+    desc("Empties the build directory.");
+    task("clean", function () {
+        startLog("reset-build-directory");
+        log("Removing '" + BUILD_DIR + "'..");
+        exec("rm -rf " + BUILD_DIR, function () {
+            endLog();
+            complete();
+        });
+    }, true);
 
-    log("Removing '" + BUILD_DIR + "'..");
-    exec("rm -rf " + BUILD_DIR, function () {
-        log("Creating '" + BUILD_DIR + "'..");
+    desc("Copies samples to the output directory.");
+    task("copy-samples", function () {
+        log("Copying samples to '" + BUILD_DIR + "'..");
         exec("cp -r " + SRC_DIR + " " + BUILD_DIR, function () {
             endLog();
             complete();
         });
-    });
-}, true);
+    }, true);
 
-task ("process-samples", function () {
-    startLog("process-samples");
+    desc("Goes through all the samples in the output directory and processes them.");
+    task("process-samples", function () {
+        startLog("process-samples");
+        fs.readdirSync(SAMPLES_DIR).forEach(function (filename) {
+            var filetype = getFileType(filename);
+            startLog("Found file " + filename + " of type " + filetype);
+            if (filetype === "html") {
+                builder.processSample(filename, SAMPLES_DIR);
+            }
+            endLog();
+        });
 
-    fs.readdirSync(SAMPLES_DIR).forEach(function (filename) {
-        var filetype = getFileType(filename);
-        startLog("Found file " + filename + " of type " + filetype);
-        if (filetype === "html") {
-            processSample(filename);
-        }
         endLog();
     });
 
-    endLog();
-});
-
-task ("clean-samples", function () {
-    startLog("clean-samples");
-
-    endLog();
-});
-
-function processSample (filename) {
-    log("Writing sample " + filename + "..");
-    var path = SAMPLES_DIR + "/" + filename,
-        fileContents = fs.readFileSync(path, "utf-8");
-    fileContents = expandScripts(fileContents);
-    fileContents = expandCSS(fileContents);
-    fs.writeFileSync(path, fileContents, "utf-8");
-}
-
-function expandScripts (inputString) {
-    log("Expanding all script tags..");
-    logIntend++;
-    inputString = expandFileReferences({
-        inputString: inputString,
-        regex: /<script src="([a-zA-Z\.\/]*)"><\/script>/g,
-        addStart: '\n<script type="text/javascript">\n\n',
-        addEnd: '\n</script>'
-    });
-    logIntend--;
-    return inputString;
-}
-
-function expandCSS (inputString) {
-    log("Expanding linked CSS..");
-    logIntend++;
-    inputString = expandFileReferences({
-        inputString: inputString,
-        regex: /<link href="([a-zA-Z\.\/]*)" rel="stylesheet" type="text\/css">/g,
-        addStart: '\n<style type="text/css">\n\n',
-        addEnd: '\n</style>'
-    });
-    logIntend--;
-    return inputString;
-}
-
-function expandFileReferences (options) {
-    options = options || {};
-    var inputString = options.inputString,
-        regex = options.regex,
-        addStart = options.addStart,
-        addEnd = options.addEnd;
-    var scriptTag, matchArray = [];
-
-    while (scriptTag = regex.exec(inputString)) {
-        var tag = scriptTag[0];
-        var src = SAMPLES_DIR + "/" + scriptTag[1];
-        matchArray.push({
-            pos: inputString.indexOf(tag),
-            tag: tag,
-            src: src
-        });
+    function getFileType (filename) {
+        var matches = filename.match(/\.(.*)$/);
+        return matches ? matches[1] : null;
     }
 
-    for (var i = matchArray.length-1; i >= 0; i--) {
-        var match = matchArray[i];
-        log(match.src);
-        var content = fs.readFileSync(match.src);
-        inputString = inputString.substr(0, match.pos) + addStart + content + addEnd + inputString.substr(match.pos + match.tag.length);
-    }
+    var logIntend = 0;
+    GLOBAL.log = function (msg) {
+        var intend = "";
+        for (var i = 0; i < logIntend; i++) { intend += "  "; }
+        console.log(intend + msg);
+    };
 
-    return inputString;
-}
+    GLOBAL.startLog = function (msg) {
+        log(msg);
+        logIntend++;
+    };
 
-function getFileType (filename) {
-    var matches = filename.match(/\.(.*)$/);
-    return matches ? matches[1] : null;
-}
-
-var logIntend = 0;
-function log (msg) {
-    var intend = "";
-    for (var i = 0; i < logIntend; i++) { intend += "  "; }
-    console.log(intend + msg);
-}
-
-function startLog (msg) {
-    log(msg);
-    logIntend++;
-}
-
-function endLog () {
-    logIntend--;
-}
+    GLOBAL.endLog = function () {
+        logIntend--;
+    };
+})();
---
+++ builder.js
@@ -0,0 +1,70 @@
+(function () {
+
+    var fs = require("fs");
+    
+    function expandScripts (inputString, directory) {
+        startLog("Expanding all script tags..");
+        inputString = expandFileReferences({
+            inputString: inputString,
+            regex: /<script src="([a-zA-Z\.\/]*)"><\/script>/g,
+            addStart: '\n<script type="text/javascript">\n\n',
+            addEnd: '\n</script>',
+            directory: directory
+        });
+        endLog();
+        return inputString;
+    }
+
+    function expandCSS (inputString, directory) {
+        startLog("Expanding linked CSS..");
+        inputString = expandFileReferences({
+            inputString: inputString,
+            regex: /<link href="([a-zA-Z\.\/]*)" rel="stylesheet" type="text\/css">/g,
+            addStart: '\n<style type="text/css">\n\n',
+            addEnd: '\n</style>',
+            directory: directory
+        });
+        endLog();
+        return inputString;
+    }
+
+    function expandFileReferences (options) {
+        options = options || {};
+        var inputString = options.inputString,
+            regex = options.regex,
+            addStart = options.addStart,
+            addEnd = options.addEnd,
+            directory = options.directory;
+        var scriptTag, matchArray = [];
+
+        while (scriptTag = regex.exec(inputString)) {
+            var tag = scriptTag[0];
+            var src = directory + "/" + scriptTag[1];
+            matchArray.push({
+                pos: inputString.indexOf(tag),
+                tag: tag,
+                src: src
+            });
+        }
+
+        for (var i = matchArray.length-1; i >= 0; i--) {
+            var match = matchArray[i];
+            log(match.src);
+            var content = fs.readFileSync(match.src);
+            inputString = inputString.substr(0, match.pos) + addStart + content + addEnd + inputString.substr(match.pos + match.tag.length);
+        }
+
+        return inputString;
+    }
+
+    exports.processSample = function (filename, directory) {
+        var path = directory + "/" + filename;
+        log("Writing sample " + path + "..");
+        var fileContents = fs.readFileSync(path, "utf-8");
+        fileContents = expandScripts(fileContents, directory);
+        fileContents = expandCSS(fileContents, directory);
+        fs.writeFileSync(path, fileContents, "utf-8");
+    };
+
+
+})();
