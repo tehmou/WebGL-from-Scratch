6d9d40ff36a59481d7885cd988a3f154a4734fd7
Timo Tuominen <timo.tuominen@gmail.com>
2012-02-21 18:21:05 +0000

    Update upload script.

--- update-pages.sh
+++ update-pages.sh
@@ -1,7 +1,19 @@
-jake
-cp -r build/* ../WebGL-from-Scratch-pages/*
+echo "empty old files.."
+rm -rf ../WebGL-from-Scratch-pages/*
+
+echo "copying files.."
+cp -r build/* ../WebGL-from-Scratch-pages/
+
+echo "changing folder.."
 cd ../WebGL-from-Scratch-pages
+
+echo "checkout gh-pages.."
 git checkout gh-pages
+
+echo "add and commit.."
+git add . -u
 git add .
 git commit -m "Build from master."
+
+echo "push"
 git push origin gh-pages
