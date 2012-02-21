a56dd2b0e2dc7b9597b65db5d072e34b5c07b6cf
Timo Tuominen <timo.tuominen@gmail.com>
2012-02-21 23:23:58 +0000

    Add work around for INDEX_SIZE_ERR: DOM Exception 1 error on first page load.

--- build/samples/canvasInitializePlainDemo.js
+++ build/samples/canvasInitializePlainDemo.js
@@ -3,31 +3,33 @@
  * ------------
  *
  * Initialize a 2D canvas and use the function defined in
- * timotuominen.webgl.canvasGeneratorFunction
- * to fill the screen.
+ * timotuominen.webgl.canvasGeneratorFunction to fill the screen.
+ *
+ * The timeout is a workaround for a strange "INDEX_SIZE_ERR: DOM Exception 1"
+ * but that happened only the first time the full screen link was opened.
  *
  */
 window.onload = function initialize () {
-    var canvas = document.createElement("canvas");
+    setTimeout(function () {
+        var canvas = document.createElement("canvas");
+        document.body.appendChild(canvas);
+        var ctx = canvas.getContext('2d');
 
-    document.body.appendChild(canvas);
-    var ctx = canvas.getContext('2d');
+        var w = canvas.width = window.innerWidth;
+        var h = canvas.height = window.innerHeight;
+        var imageData = ctx.createImageData(w, h);
 
-    var w = canvas.width = window.innerWidth;
-    var h = canvas.height = window.innerHeight;
-    var imageData = ctx.createImageData(w, h);
-
-    for (var x = 0; x < w; x++) {
-        for (var y = 0; y < h; y++) {
-            var idx = (x + y * w) * 4;
-
-            var c = (timotuominen.webgl.canvasGeneratorFunction(x, y) + 1)*128;
-            imageData.data[idx + 0] = c;
-            imageData.data[idx + 1] = c;
-            imageData.data[idx + 2] = c;
-            imageData.data[idx + 3] = 255;
+        for (var x = 0; x < w; x++) {
+            for (var y = 0; y < h; y++) {
+                var idx = (x + y * w) * 4;
+                var c = (timotuominen.webgl.canvasGeneratorFunction(x, y) + 1)*128;
+                imageData.data[idx + 0] = c;
+                imageData.data[idx + 1] = c;
+                imageData.data[idx + 2] = c;
+                imageData.data[idx + 3] = 255;
+            }
         }
-    }
 
-    ctx.putImageData(imageData, 0, 0);
+        ctx.putImageData(imageData, 0, 0);
+    }, 100);
 };
